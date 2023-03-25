13f669a7a9ee5572d1f5f5dfb8091b56de823a9c
aviwatwani <2017.avi.watwani@ves.ac.in>
2023-03-25 17:19:32 +0530

    section 4 done

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -11,4 +11,39 @@ class Tasks extends BaseController
 
         return view('Tasks/index', ['tasks' => $data]);
     }
+
+    public function show($id)
+    {
+        $model = new \App\Models\TaskModel;
+        $task = $model->find($id);
+
+        return view('Tasks/show', ['task' => $task]);
+    }
+
+    public function new()
+	{
+		return view('Tasks/new');
+	}
+	
+	public function create()
+	{
+        $model = new \App\Models\TaskModel;
+		
+		$result = $model->insert([
+			'description' => $this->request->getPost("description")
+		]);
+		
+        if ($result === false) {
+			
+            return redirect()->back()
+							 ->with('errors', $model->errors())
+							 ->with('warning', 'Invalid data');
+		
+        } else {
+		
+			return redirect()->to("/tasks/show/$result")
+							 ->with('info', 'Task created successfully');
+			
+		}
+	}
 }
