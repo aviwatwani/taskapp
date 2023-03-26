d9764b3ccce64e9e24b5fd2cfc4ff33e30378fb4
aviwatwani <2017.avi.watwani@ves.ac.in>
2023-03-26 14:08:45 +0530

    added user model, login, signup

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,9 +1,13 @@
-<?= $this->extend('layouts/default') ?>
+<?= $this->extend("layouts/default") ?>
 
-<?= $this->section('title') ?>
-    Home
-<?= $this->endSection() ?>
+<?= $this->section("title") ?>Home<?= $this->endSection() ?>
 
-<?= $this->section('content') ?>
+<?= $this->section("content") ?>
+
     <h1>Welcome</h1>
+
+    <a href="<?= site_url("/signup") ?>">Sign up</a>
+    
+    <a href="<?= site_url("/login") ?>">Log in</a>
+
 <?= $this->endSection() ?>
